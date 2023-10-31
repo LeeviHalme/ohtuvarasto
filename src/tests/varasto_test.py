3d8e41e993031411b1c4b7ff2a09f6b5e9761b46
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-10-31 19:09:31 +0200

    add tests to get 100% branch coverage on varasto.py

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,45 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisaa_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(-8)
+
+        # varastossa pitäisi olla tilaa saman verran kuin aikaisemminkin (10)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+    
+    def test_lisaa_ylitayteen(self):
+        self.varasto.lisaa_varastoon(100)
+
+        # varastossa pitäisi olla vain niin monta tavaraa, kuin tilavuus on
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_negatiivinen_maara(self):
+        maara = self.varasto.ota_varastosta(-10)
+
+        # varastossa pitäisi olla tilaa saman verran kuin aikaisemminkin (10)
+        self.assertAlmostEqual(maara, 0)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_ota_enemman_kuin_saldo(self):
+        self.varasto.lisaa_varastoon(50)
+        maara = self.varasto.ota_varastosta(100)
+
+        # varastossa pitäisi olla tilaa ja määrä pitäisi olla 10 (koko tilavuus)
+        self.assertAlmostEqual(maara, 10)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_tulostus(self):
+        self.assertAlmostEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+
+    def test_negatiivinen_tilavuus(self):
+        varasto = Varasto(-10, 5)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_negatiivinen_saldo(self):
+        varasto = Varasto(10, -5)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_alkusaldo_yli_tilavuuden(self):
+        varasto = Varasto(10, 50)
+        self.assertAlmostEqual(varasto.saldo, 10)
